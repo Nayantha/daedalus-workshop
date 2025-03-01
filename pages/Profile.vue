69025f66bd1dd2df4761cf0045d2ec071da19d53
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-01 20:45:25 +0530

    add user profile page.

---
+++ pages/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+<header>
+    <template v-if="loggedIn">
+        <p>Greetings, {{user?.username}}</p>
+        <button @click="logout"></button>
+    </template>
+    <p v-else>Guest</p>
+</header>
+</template>
+
+<script lang="ts" setup>
+const {loggedIn, user, clear} = useUserSession();
+
+const logout = () => {
+  clear();
+}
+
+useHead({
+    title: "User profile"
+})
+</script>
+
+<style scoped>
+
+</style>
