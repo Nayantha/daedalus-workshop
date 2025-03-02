eac3487bfd408f2f4867df25ad02052aab679c4b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-02 17:34:46 +0530

    create new user types for type conversions.

---
+++ types/User.ts
@@ -0,0 +1,18 @@
+import { User } from '@prisma/client';
+
+export interface RegisterDTO {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export interface LoginDTO {
+    email: string;
+    password: string;
+}
+
+// For creating new users
+export type CreateUserDB = Omit<User, 'id'>;
+export type UpdateUserDB = Partial<Omit<User, 'id'>>;
+export type PublicUser = Omit<User, 'password'|'salt'>;
