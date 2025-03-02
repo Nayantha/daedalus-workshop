9735c4b7c7fdace3705f7f276982d18083dc0249
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-02 13:32:23 +0530

    remove unrelated logic and template.

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,18 +1,7 @@
 <template>
-    <div v-if="loggedIn">
-        <h1>Welcome {{ user.login }}!</h1>
-        <p>Logged in since {{ session.loggedInAt }}</p>
-        <button @click="clear">Logout</button>
-    </div>
-    <div v-else>
-        <h1>Not logged in</h1>
-        <a href="/auth/github">Login with GitHub</a>
-        <button @click="openInPopup('/auth/github')">Login with GitHub</button>
-    </div>
 </template>
 
 <script lang="ts" setup>
-const { loggedIn, user, session, fetch, clear, openInPopup } = useUserSession()
 
 useHead({
     title: "Login Page"
