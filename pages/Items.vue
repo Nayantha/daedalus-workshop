4032591e264a55407aa2e4422d2acdc6a19c02fe
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-04 17:34:54 +0530

    add rendering logic of fetch items.

---
+++ pages/Items.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-if="error">{{ error.message }}</div>
+        <div v-else-if="!items">Loading...</div>
+        <div v-else>
+            <div v-for="item in items" :key="item.id">
+                {{ item }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const { data: items, error } = await useFetch('/api/items');
+</script>
+
+<style scoped>
+
+</style>
