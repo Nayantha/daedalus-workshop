fd050eb5cfd5bbb82355956e257ee868e7661904
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-04 18:47:48 +0530

    extract quires and employ the new service.

--- server/api/items/index.get.ts
+++ server/api/items/index.get.ts
@@ -1,11 +1,32 @@
-import prisma from "~/lib/prisma";
 import { H3Event } from "h3";
+import { fetchItems } from "~/server/services/itemService";
 
 export default defineEventHandler(async (event: H3Event) => {
     try {
-        const items = await prisma.item.findMany();
-        // console.log(items);
-        return items;
+        const query = getQuery(event);
+        const page = parseInt(query.page as string) || 1;
+        const pageSize = parseInt(query.pageSize as string) || 10;
+        const filterName = query.filterName as string;
+        const minPrice = parseFloat(query.minPrice as string);
+        const maxPrice = parseFloat(query.maxPrice as string);
+        const rarity = query.rarity as string;
+
+        const { items, totalItems } = await fetchItems({
+            page,
+            pageSize,
+            filterName,
+            minPrice,
+            maxPrice,
+            rarity,
+        });
+
+        return {
+            items,
+            totalItems,
+            totalPages: Math.ceil(totalItems / pageSize),
+            currentPage: page,
+        };
+
     } catch (error) {
         console.error(error);
         throw createError({ statusCode: 500, statusMessage: "Internal Server Error" });
