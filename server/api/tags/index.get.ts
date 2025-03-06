4a7570d0c61345b917c37f1aa25231399e289fd5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-06 16:16:33 +0530

    add end point to fetch tags from db.

---
+++ server/api/tags/index.get.ts
@@ -0,0 +1,13 @@
+import { H3Event } from "h3";
+import prisma from "~/lib/prisma";
+
+export default defineEventHandler(async (event: H3Event) => {
+    try {
+        const tags = await prisma.tag.findMany();
+        console.log(tags);
+        return { tags };
+    } catch (error) {
+        console.error(error);
+        throw createError({ statusCode: 500, statusMessage: "Internal Server Error" });
+    }
+})
