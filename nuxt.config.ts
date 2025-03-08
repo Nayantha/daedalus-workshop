559cf7b7ec2846505b415395fbb247e94331c5a9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-08 15:24:09 +0530

    add tailwind module.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', 'nuxt-auth-utils', "@prisma/nuxt"]
+  modules: [
+    '@pinia/nuxt',
+    'nuxt-auth-utils',
+    "@prisma/nuxt",
+    '@nuxtjs/tailwindcss'
+  ]
 })
