beba60e8d1b29adb9feb780a4cd365a646f7bbd8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-08 16:10:15 +0530

    add button to toggle theme.

---
+++ components/ThemeToggleBotton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button aria-label="Color Mode"
+                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent bg-blue-800 dark:bg-white mt-4 lg:mt-0"
+                @click="changeColor">
+            <ColorScheme placeholder="...">
+                <Icon v-if="colorMode.value === 'dark'" class="text-xl text-black" name="heroicons-outline:moon"/>
+                <Icon v-else class="text-xl" name="heroicons-outline:sun"/>
+            </ColorScheme>
+        </button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const colorMode = useColorMode();
+const changeColor = () => (colorMode.preference = (colorMode.value === 'light' ? 'dark' : 'light'))
+</script>
+
+<style scoped>
+</style>
