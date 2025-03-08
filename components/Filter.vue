ea7ab4a260d78e053250d1267f73ae0399c53622
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-08 11:50:49 +0530

    add tag fetching logic here.

---
+++ components/Filter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div v-if="error">{{ error.message }}</div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const { data, error } = await useFetch(("/api/tags"));
+
+const tags = ref(data.value?.tags || {});
+
+</script>
+<style scoped>
+
+</style>
