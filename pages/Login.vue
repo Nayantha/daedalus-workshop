4cc389a54cd97a00a71d2ac8d23d23b4fd8d3855
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-09 18:32:59 +0530

    add text color in dark mode.

--- pages/Login.vue
+++ pages/Login.vue
@@ -5,7 +5,7 @@
              style="background-image: url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80');">
         </div>
         <div class="w-full px-6 py-8 md:px-8 lg:w-1/2">
-            <p class="mt-3 text-xl text-center">
+            <p class="mt-3 text-xl text-center dark:text-white">
                 Welcome back!
             </p>
             <div class="flex items-center mt-6 -mx-2">
--- pages/Register.vue
+++ pages/Register.vue
@@ -3,10 +3,9 @@
         class="flex w-full mt-4 max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
         <div class="hidden bg-cover lg:block lg:w-1/2"
              style="background-image: url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80');">
-            Image
         </div>
         <div class="w-full px-6 py-8 md:px-8 lg:w-1/2">
-            <p class="mt-3 text-xl text-center">
+            <p class="mt-3 text-xl text-center dark:text-white">
                 Welcome!
             </p>
             <div class="flex items-center mt-6 -mx-2">
