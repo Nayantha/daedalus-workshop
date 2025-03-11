307998a42a5ce5c5f61045f206f74a00cd62e6ff
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-11 12:18:21 +0530

    replace backend call with enum.

--- components/Filter.vue
+++ components/Filter.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
-        <div v-if="error">{{ error.message }}</div>
+        Filter Menu
     </div>
+    <a v-for="(tag, i) in TAGS"
+       :key="i"
+       :data-dui-tab-target="`tab${i}-group`"
+       :href="`?tag=${tag.toLowerCase()}`"
+       class="text-sm active inline-block py-2 px-4 text-stone-800 transition-all duration-300 relative z-1 mr-1">
+        {{ tag.toLowerCase() }}
+    </a>
 </template>
 
 <script lang="ts" setup>
-const { data, error } = await useFetch(("/api/tags"));
+import { TagType } from '@prisma/client';
 
-const tags = ref(data.value?.tags || {});
+const TAGS = Object.values(TagType || {});
 
+console.log(TagType, TAGS);
 </script>
 <style scoped>
 
