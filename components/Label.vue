793b8e310f5dcb968e28068cf4c96e333659f459
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-14 19:27:28 +0530

    add custom component from template.

---
+++ components/Label.vue
@@ -0,0 +1,19 @@
+<template>
+    <label
+            :class="[
+      'text-sm font-medium leading-none select-none',
+      'group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50',
+      'peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+      className
+    ]"
+            v-bind="$attrs"
+    >
+        <slot/>
+    </label>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+    className?: string
+}>();
+</script>
