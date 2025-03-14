dc2b20331ac07ee61307e396992327fa944e7192
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-14 19:25:08 +0530

    update tags and classes using a template.

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,89 +1,96 @@
 <template>
-    <div
-        class="flex w-full mt-4 max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-        <div class="hidden bg-cover lg:block lg:w-1/2"
-             style="background-image: url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80');">
-        </div>
-        <div class="w-full px-6 py-8 md:px-8 lg:w-1/2">
-            <p class="mt-3 text-xl text-center dark:text-white">
-                Welcome back!
-            </p>
-            <OAuthAuthentication/>
-            <div class="flex items-center justify-between mt-4">
-                <span class="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
+    <section class="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-zinc-900">
 
-                <div class="text-xs text-center text-gray-500 uppercase dark:text-gray-400">or
-                    login
-                    with email
+        <form
+            class="bg-muted m-auto h-fit w-full max-w-sm overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:border-zinc-700 dark:[--color-muted:var(--color-zinc-800)]"
+            @submit="onSubmit"
+        >
+
+            <div class="bg-card -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6 dark:border-zinc-700">
+                <div class="text-center">
+                    <router-link
+                        aria-label="go home"
+                        class="mx-auto block w-fit"
+                        to="/"
+                    >
+                        <Logo class="text-zinc-900 dark:text-white"/>
+                    </router-link>
+                    <h1 class="mb-1 mt-4 text-xl font-semibold dark:text-white">Sign In to Daedalus' Workshop</h1>
+                    <p class="text-sm text-zinc-600 dark:text-zinc-400">Welcome back! Sign in to continue</p>
                 </div>
 
-                <span class="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
-            </div>
-            <form class="space-y-6" @submit="onSubmit">
-                <div v-if="error" class="bg-red-50 text-red-600 p-4 rounded-lg mt-4">
-                    {{ error }}
+                <OAuthAuthentication/>
+
+                <div class="my-6 grid grid-cols-[1fr_auto_1fr] items-center gap-3">
+                    <hr class="border-dashed dark:border-zinc-700"/>
+                    <span class="text-muted-foreground text-xs dark:text-zinc-400">Or continue With</span>
+                    <hr class="border-dashed dark:border-zinc-700"/>
                 </div>
 
-                <div v-if="success" class="bg-green-50 text-green-600 p-4 rounded-lg mt-4">
-                    Login successfully completed. Redirecting to home page....
-                </div>
+                <div class="mt-6 space-y-6">
+                    <div class="space-y-2">
+                        <Label class="block text-sm dark:text-zinc-300" for="email">
+                            Email
+                        </Label>
+                        <input
+                            id="email"
+                            v-model="email"
+                            :class="{ 'border-red-500': errors.email }"
+                            autocomplete="email"
+                            class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                            name="email"
+                            required
+                            type="email"
+                        />
+                        <p v-if="errors.email" class="mt-1 text-sm text-red-600">{{ errors.email }}</p>
+                    </div>
 
-                <div class="mt-4">
-                    <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" for="email">
-                        Email address
-                    </label>
-                    <input
-                        id="email"
-                        v-model="email"
-                        :class="{ 'border-red-500': errors.email }"
-                        autocomplete="email"
-                        class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-                        name="email"
-                        required
-                        type="email"
-                    />
-                    <p v-if="errors.email" class="mt-1 text-sm text-red-600">{{ errors.email }}</p>
-                </div>
+                    <div class="space-y-0.5">
+                        <div class="flex items-center justify-between">
+                            <Label class="text-title text-sm dark:text-zinc-300" for="password">
+                                Password
+                            </Label>
+                        </div>
+                        <input
+                            id="password"
+                            v-model="password"
+                            :class="{ 'border-red-500': errors.password }"
+                            autocomplete="current-password"
+                            class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                            name="password"
+                            required
+                            type="password"
+                        />
+                        <p v-if="errors.password" class="mt-1 text-sm text-red-600">{{ errors.password }}</p>
+                    </div>
 
-                <div class="mt-4">
-                    <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-                           for="password">
-                        Password
-                    </label>
-                    <input
-                        id="password"
-                        v-model="password"
-                        :class="{ 'border-red-500': errors.password }"
-                        autocomplete="current-password"
-                        class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-                        name="password"
-                        required
-                        type="password"
-                    />
-                    <p v-if="errors.password" class="mt-1 text-sm text-red-600">{{ errors.password }}</p>
-                </div>
-
-                <div class="mt-4">
                     <button
-                            :disabled="loading"
-                            class="flex w-full justify-center rounded-md transition-colors duration-300 transform bg-gray-800 px-6 py-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed dark:text-black dark:bg-white"
-                            type="submit"
+                        :disabled="loading"
+                        class="flex w-full justify-center rounded-md transition-colors duration-300 transform bg-gray-800 px-6 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed dark:text-black dark:bg-indigo-600 dark:hover:bg-indigo-700"
+                        type="submit"
                     >
                         <span v-if="loading">Processing...</span>
                         <span v-else>Sign in</span>
                     </button>
                 </div>
-            </form>
-            <div class="flex items-center justify-between mt-4">
-                <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
 
-                <a class="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline" href="/register">or
-                    register</a>
-
-                <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
             </div>
-        </div>
-    </div>
+
+            <div class="p-3 border-t dark:border-zinc-700">
+                <p class="text-accent-foreground text-center text-sm text-zinc-600 dark:text-zinc-400">
+                    Don't have an account ?
+                    <Button class="px-2" type="submit" variant="link">
+                        <router-link
+                            class="text-indigo-600 dark:text-indigo-400 hover:underline"
+                            to="/register"
+                        >
+                            Create account
+                        </router-link>
+                    </Button>
+                </p>
+            </div>
+        </form>
+    </section>
 </template>
 
 <script lang="ts" setup>
