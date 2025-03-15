e27a4f3a050d9d1664ebf32f5a6dc6efee2d66b6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-15 13:32:46 +0530

    add navigation menu links.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -21,51 +21,26 @@
                 />
             </button>
             <!--Nav Links-->
-            <PopoverGroup class="hidden lg:flex lg:gap-x-12">
-                <Popover class="relative">
-                    <PopoverButton class="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
-                        Product
-                        <ChevronDownIcon aria-hidden="true" class="size-5 flex-none text-gray-400"/>
-                    </PopoverButton>
-
-                    <transition enter-active-class="transition ease-out duration-200"
-                                enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0"
-                                leave-active-class="transition ease-in duration-150"
-                                leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-                        <PopoverPanel
-                                class="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white ring-1 shadow-lg ring-gray-900/5">
-                            <div class="p-4">
-                                <div v-for="item in products" :key="item.name"
-                                     class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50">
-                                    <div class="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                        <component :is="item.icon" aria-hidden="true"
-                                                   class="size-6 text-gray-600 group-hover:text-indigo-600"/>
-                                    </div>
-                                    <div class="flex-auto">
-                                        <a :href="item.href" class="block font-semibold text-gray-900">
-                                            {{ item.name }}
-                                            <span class="absolute inset-0"/>
-                                        </a>
-                                        <p class="mt-1 text-gray-600">{{ item.description }}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                                <a v-for="item in callsToAction" :key="item.name" :href="item.href"
-                                   class="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100">
-                                    <component :is="item.icon" aria-hidden="true"
-                                               class="size-5 flex-none text-gray-400"/>
-                                    {{ item.name }}
-                                </a>
-                            </div>
-                        </PopoverPanel>
-                    </transition>
-                </Popover>
-
-                <a class="text-sm/6 font-semibold text-gray-900" href="#">Features</a>
-                <a class="text-sm/6 font-semibold text-gray-900" href="#">Marketplace</a>
-                <a class="text-sm/6 font-semibold text-gray-900" href="#">Company</a>
-            </PopoverGroup>
+            <div
+                :class="[
+                'bg-white dark:bg-zinc-900 lg:dark:bg-transparent',
+                'mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none',
+                { 'block lg:flex': mobileMenuOpen }
+              ]"
+            >
+                <div class="lg:pr-4">
+                    <ul class="space-y-6 text-base lg:flex lg:gap-8 lg:space-y-0 lg:text-sm">
+                        <li v-for="(item, index) in products" :key="index">
+                            <router-link
+                                :to="item.href"
+                                class="text-zinc-600 hover:text-indigo-600 dark:text-zinc-400 dark:hover:text-indigo-400 block duration-150"
+                            >
+                                {{ item.name }}
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <!--Nav Login/SignUp + Theme Toggle-->
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
                 <a class="text-sm/6 font-semibold text-gray-900" href="/login">Log in <span
@@ -133,17 +108,7 @@
 
 <script setup>
 import { ref } from 'vue'
-import {
-    Dialog,
-    DialogPanel,
-    Disclosure,
-    DisclosureButton,
-    DisclosurePanel,
-    Popover,
-    PopoverButton,
-    PopoverGroup,
-    PopoverPanel,
-} from '@headlessui/vue'
+import { Dialog, DialogPanel, Disclosure, DisclosureButton, DisclosurePanel, } from '@headlessui/vue'
 import {
     ArrowPathIcon,
     ChartPieIcon,
